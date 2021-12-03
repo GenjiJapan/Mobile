68ad4f1b48fa410b98f1df3ab2574e2d39f87ca3
Le <nghi.le@bigdutchman.com>
2021-12-03 16:17:21 +0700

    nghi 3/12

--- App.js
+++ App.js
@@ -2,17 +2,32 @@
 import { createAppContainer } from "react-navigation";
 import Home from "./screens/Home/Home";
 import Settings from "./screens/Home copy/Settings";
-// import Details from "./screens/Detail/Detail";
+import Details from "./screens/Detail/Detail";
 import News from "./screens/News/News";
 import { Platform } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createDrawerNavigator } from "react-navigation-drawer";
+import { createStackNavigator } from "react-navigation-stack";
+import React, { useState } from "react";
+import { Text, View, FlatList } from "react-native";
+import styles from "./screens/Styles";
 
-const { createNavigator } = Platform.select({
-  ios: { createNavigator: createBottomTabNavigator },
-  android: { createNavigator: createDrawerNavigator },
-});
-
-export default createAppContainer(
-  createNavigator({ Home, News, Settings }, { initialRouteName: "Home" })
+const Nav = createAppContainer(
+  createStackNavigator({ Home, Details }, { initialRouteName: "Home" })
 );
+
+const data = new Array(100).fill(null).map((v, i) => ({
+  key: i.toString(),
+  value: `Item ${i}`,
+}));
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text>{item.value} </Text>}
+      />
+    </View>
+  );
+}
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,10 +2,39 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import styles from "../Styles";
 
-function Home({ navigation }) {
+function Home({ navigation, screenProps: { stock } }) {
   return (
     <View style={styles.container}>
-      <Text>Home Content</Text>
+      <Button
+        title={`First Item (${stock.first})`}
+        onPress={() =>
+          navigation.navigate("Details", {
+            id: "first ",
+            title: "First Item",
+            content: "First Item Content",
+          })
+        }
+      />
+      <Button
+        title={`First Item (${stock.second})`}
+        onPress={() =>
+          navigation.navigate("Details", {
+            id: "second ",
+            title: "Second Item",
+            content: "Second Item Content",
+          })
+        }
+      />
+      <Button
+        title={`First Item (${stock.third})`}
+        onPress={() =>
+          navigation.navigate("Details", {
+            id: "third ",
+            title: "Third Item",
+            content: "Third Item Content",
+          })
+        }
+      />
     </View>
   );
 }
